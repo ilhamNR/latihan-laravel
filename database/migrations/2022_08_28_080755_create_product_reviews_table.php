dfbd3ad9c1597012f7c20cae40d5d73688f73657
ilhamNR <ilhamnurramadhan19@gmail.com>
2022-08-28 15:19:29 +0700

    fix typo

--- database/migrations/2022_08_28_080755_create_product_reviews_table.php
+++ database/migrations/2022_08_28_080755_create_product_reviews_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product_review', function (Blueprint $table) {
+        Schema::create('product_reviews', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('score');
